a2f997a7835818fae034987d8351fa5605e5b1a7
Miguel Ángel Rojas <mrojastapias@gmail>
2019-03-05 23:21:31 -0500

    proyecto

---
+++ Demo/Assets/spin.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spin : MonoBehaviour {
+
+    public float speed = 0;
+    GameObject palanca;
+
+   // Use this for initialization
+   void Start () {
+        palanca = GameObject.Find("EsferaControl");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        speed = palanca.transform.position.x;
+        speed = (float)(speed-98.5) * 300;
+
+        transform.Rotate(new Vector3(1,0,1),speed*Time.deltaTime);
+	}
+}
